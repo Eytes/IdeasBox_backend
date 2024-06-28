b368f85649d688c63c433da4b70ab6d8b6006d89
Eytes <yura.shamanov2000@bk.ru>
2024-06-28 14:57:32 +0300

    удаление слова mongo в названии класса для названия коллекции

--- src/core/db/mongo.py
+++ src/core/db/mongo.py
@@ -9,7 +9,9 @@ class MongoRepositoryFactory:
 
     def create_collection_and_get_repository[
         MongoRepository
-    ](self, repository: Type[MongoRepository]) -> MongoRepository:
+    ](self, repository: Type[MongoRepository],) -> MongoRepository:
         """Автоматически создается коллекция и набор CRUD операций для этой коллекции"""
-        collection_name = repository.__name__.lower().replace("repository", "")
+        collection_name = (
+            repository.__name__.lower().replace("mongo", "").replace("repository", "")
+        )
         return repository(self.__database[collection_name])
