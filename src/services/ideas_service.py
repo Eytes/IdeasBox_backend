c12d43b48ddc4cc26519c7c3d5b0f5d14983645e
Eytes <yura.shamanov2000@bk.ru>
2024-06-30 21:18:34 +0300

    создание заготовки для паттерна Service для взаимодействия с идеями

---
+++ src/services/ideas_service.py
@@ -0,0 +1,24 @@
+from core.base_types import ItemId
+from core.db.abstract_repository import Repository
+from schemas.idea import IdeaSchema, IdeaBaseSchema, IdeaUpdateSchema
+
+
+class IdeasService:
+    def __init__(self, repository: Repository) -> None:
+        self.repository = repository
+
+    async def get_own_ideas(self) -> list[IdeaSchema]:
+        pass
+
+    async def create_idea(self, new_idea: IdeaBaseSchema) -> IdeaSchema:
+        pass
+
+    async def update_own_idea(
+        self,
+        idea_id: ItemId,
+        new_idea_data: IdeaUpdateSchema,
+    ) -> IdeaSchema:
+        pass
+
+    def delete_own_idea(self, idea_id: ItemId) -> IdeaBaseSchema:
+        pass
